eeb59f2ecaaddee53f41e247fe4da12659a276ad
Prabakaran.M <karanpk0108@gmail.com>
2023-09-22 18:13:20 +0530

    HeroSection

---
+++ app/src/components/HeroSection.jsx
@@ -0,0 +1,70 @@
+"use client";
+import React from "react";
+import { TypeAnimation } from "react-type-animation";
+
+const HeroSection = () => {
+  return (
+    <section>
+      <div className="grid grid-cols-1 sm:grid-cols-12 ">
+        <div className="col-span-7 place-self-center">
+          <h1 className="text-white mb-4 sm:text-left text-4xl sm:text-5xl lg:text-6xl font-extrabold">
+            <span className=" text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500">
+              Hello, I'm{" "}
+            </span>
+            <br />
+            <TypeAnimation
+              sequence={[
+                // Same substring at the start will only be typed out once, initially
+                "Praba!",
+                1000, // wait 1s before replacing "Mice" with "Hamsters"
+                "Web Developer",
+                1000,
+                "React.JS Developer",
+                1000,
+              ]}
+              wrapper="span"
+              speed={20}
+              style={{ display: "inline-block" }}
+              repeat={Infinity}
+            />
+          </h1>
+          <p
+            className="text-gray-500 text-base sm:text-left mb-6 sm:text-lg
+          lg:text-xl"
+          >
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+            porta velit id bibendum aliquet.
+          </p>
+
+          <div>
+            <button className="px-6 py-3 mr-2 w-full sm:w-fit rounded-full bg-gradient-to-r from-green-400 to-blue-500  hover:from-zinc-400 hover:to-cyan-300 text-black">
+              Hire me
+            </button>
+            <button className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-r from-green-400 to-blue-500 hover:from-zinc-400 hover:to-cyan-300  mt-3">
+              {" "}
+              <span
+                className=" block bg-black 
+              hover:from-zinc-400 hover:to-cyan-300 rounded-full px-3 py-2"
+              >
+                Download CV
+              </span>
+            </button>
+          </div>
+        </div>
+
+        <div className="col-span-5 place-self-center mt-4  lg:mt-0">
+          <div className="rounded-full bg-gray-900 w-[270px] h-[270px] lg:w-[270px] lg:h-[270px] relative pr-24 ">
+            <img
+              src="/images/hero-image.png"
+              height={300}
+              width={200}
+              className="absolute rounded-full transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 "
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
