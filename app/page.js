b1bcd0e1ce24e3a833f028026689d380bbc7fdc2
Prabakaran.M <karanpk0108@gmail.com>
2023-09-25 16:02:16 +0530

    Emailsection

--- app/page.js
+++ app/page.js
@@ -1,6 +1,9 @@
 import Navbar from "./src/components/Navbar";
 import HeroSection from "./src/components/HeroSection";
 import AboutSection from "./src/components/AboutSection";
+import ProjectSection from "./src/components/ProjectSection";
+import EmailSection from "./src/components/EmailSection";
+import Footer from "./src/components/Footer";
 
 export default function Home() {
   return (
@@ -9,7 +12,10 @@ export default function Home() {
       <div className="container mt-24 mx-auto  px-11 py-4">
         <HeroSection />
         <AboutSection />
+        <ProjectSection />
+        <EmailSection />
       </div>
+      <Footer />
     </main>
   );
 }
