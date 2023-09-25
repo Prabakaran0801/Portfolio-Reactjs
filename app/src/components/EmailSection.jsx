9f82621a388533d3c5ac3d0e75c2ee46296cde42
Prabakaran.M <karanpk0108@gmail.com>
2023-09-25 17:24:48 +0530

    fix deployment err

--- app/src/components/EmailSection.jsx
+++ app/src/components/EmailSection.jsx
@@ -15,9 +15,8 @@ const EmailSection = () => {
       <div className="z-10">
         <h5 className="text-xl font-bold text-white my-2">Let's Connect</h5>
         <p className=" text-slate-400 mb-4 max-w-md">
-          I'm currently looking for new opportunities, my inbox is always open.
-          Whether you have a question or just want to say hi, I'll try my best
-          to get back to you!
+          I&apos;m currently looking for new opportunities, my inbox is always
+          open. Whether you have a question or just want to say hi, I&apos;ll
         </p>
         <div className="socials flex flex-row gap-2">
           <Link href="github.com">
--- app/src/components/HeroSection.jsx
+++ app/src/components/HeroSection.jsx
@@ -16,7 +16,7 @@ const HeroSection = () => {
         >
           <h1 className="text-white mb-4 sm:text-left text-4xl sm:text-5xl lg:text-6xl font-extrabold">
             <span className=" text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500">
-              Hello, I'm{" "}
+              Hello, I&apos;m{" "}
             </span>
             <br />
             <TypeAnimation
