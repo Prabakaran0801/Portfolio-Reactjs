b5729a813ae4928dda45d357a31b99d7f6d3d45a
Prabakaran <karanpk0108@gmail.com>
2025-02-10 13:57:41 +0530

    fixing sending email

--- app/api/sendEmail/route.js
+++ app/api/sendEmail/route.js
@@ -10,8 +10,8 @@ export async function POST(request) {
     // Create a transporter object using the default SMTP transport
     let transporter = nodemailer.createTransport({
       host: "smtp.gmail.com",
-      port: 587,
-      secure: false,
+      port: 465,
+      secure: true,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
